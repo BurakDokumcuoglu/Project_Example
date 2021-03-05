ec72e262426ac14a49723e9e78376a74dfb7c9a8
burakdokumcuoglu3@gmail.com <burakdokumcuoglu3@gmail.com>
2021-03-05 23:45:23 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,51 @@
+﻿using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EnityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ProductTest();
+
+            // CategoryTest();
+
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            var liste = productManager.GetProductDetails();
+            Console.WriteLine(liste.Message);
+            foreach (var item in liste.Data)
+            {
+                Console.WriteLine("Ürün adı : "+item.ProductName+"   ----   Kategorisi : "+item.CategoryName);
+            }
+            
+
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            var liste2 = categoryManager.GetAll();
+            foreach (var item in liste2)
+            {
+                Console.WriteLine("Kategori adı : " + item.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            var liste = productManager.GetByUnitPrice(1, 10);
+            foreach (var item in liste.Data)
+            {
+                Console.WriteLine(item.ProductName);
+                Console.WriteLine("Fiyatı : " + item.UnitPrice);
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Core.DataAccess
+{
+    // "class" generic kısıtlayıcısı ile referans tip alınması gerektiği belirtilir.
+    // new'lenebilir olmak zorunda
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
